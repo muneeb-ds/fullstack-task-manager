686f53fbffae2b5cc6069c6b6bc641484aa80ae1
Muneeb Hassan <muneeb.hassan@dataquartz.com>
2025-06-12 19:19:44 +0500

    dynamic backend api url from env

--- client/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace environment variables in index.html
-sed -i "s|ENV_BACKEND_API_URL|'$BACKEND_API_URL'|g" /usr/share/nginx/html/index.html
-
-# Start nginx
-nginx -g 'daemon off;' 
